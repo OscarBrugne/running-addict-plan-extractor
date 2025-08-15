9a29739fd2957e491632c42771c03f4e3bba4692
OscarBrugne <oscar.brugne@tum.de>
2025-08-15 14:12:22 +0200

    Use type hinting in __init__ files

--- running_addict_plan_extractor/data/__init__.py
+++ running_addict_plan_extractor/data/__init__.py
@@ -1,3 +1,3 @@
 from . import client, running_addict_api
 
-__all__ = ["client", "running_addict_api"]
+__all__: list[str] = ["client", "running_addict_api"]
--- running_addict_plan_extractor/data/running_addict_api/__init__.py
+++ running_addict_plan_extractor/data/running_addict_api/__init__.py
@@ -1,3 +1,3 @@
 from .api import get_half_marathon_plan
 
-__all__ = ["get_half_marathon_plan"]
+__all__: list[str] = ["get_half_marathon_plan"]
